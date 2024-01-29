e41364d1c6a5e494eb518279982a59e8e0d55016
Rene <racosta323@gmail.com>
2024-01-29 10:32:44 -0600

    complete lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,19 @@
+//import libraries needed for testing
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
+//import the component you wrote
 import Article from "../components/Article";
 
+//test the component
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug(element)
+
+  expect(element).toBeInTheDocument();
+  // expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
